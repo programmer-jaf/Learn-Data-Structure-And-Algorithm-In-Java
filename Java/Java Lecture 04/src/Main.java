85e9ab8d9ba5456b1279ff6596070045f7a3cd6e
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-06 21:19:53 +0600

    add example program demonstrating primitive data types in Java

---
+++ Java/Java Lecture 04/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("learn about primitive datatypes");
+    /*
+    1️⃣ whole number : byte,short,int,long
+    2️⃣ decimal : float,double
+    3️⃣ characters : char
+    4️⃣ booleans : boolean
+     */
+//  byte
+    byte byteValue = 127;
+    System.out.println(byteValue);
+//  short
+    short shortValue = 32000;
+    System.out.println(shortValue);
+//  int
+    int intValue = 2654054;
+    System.out.println(intValue);
+//  long
+//  ! Make sure add L at the end of the number
+    long longValue = 1254484566454646L;
+    System.out.println(longValue);
+
+//  float
+//  ! Make sure add f at the end of the number
+    float floatValue = 254.1645f;
+    System.out.println(floatValue);
+//  double
+//  ! For double, it's not mandatory to add d at the end of the number
+  double doubleValue = 21.165415665;
+  System.out.println(doubleValue);
+
+//  char
+    char charValue = 'A';
+    System.out.println(charValue);
+
+//  boolean
+    boolean booleanValue = true;
+    System.out.println(booleanValue);
+    
+  }
+}
