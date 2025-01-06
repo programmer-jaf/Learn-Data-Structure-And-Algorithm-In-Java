d91cbd44fb48814fedff4a3f00aabff5a48758ca
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-06 21:00:32 +0600

    learn about variables 🍼

---
+++ Java/Java Lecture 03/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("variables and rules while creating variables 🫙");
+    /*
+    ! what is variables ?
+    * ans : variables are container are to store values.
+    ? Example : if you want to store milk. You have to store it into a bottle 🍼.Here 🍼 is a variables
+     */
+//  int
+    int variables = 10;
+    System.out.println(variables);
+//  String
+    String milkBottle = "milk bottle 🍼";
+    System.out.println(milkBottle);
+  }
+}
