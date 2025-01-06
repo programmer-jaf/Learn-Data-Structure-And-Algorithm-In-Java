d4853f87d6c6f1831d452d1bf6c9bcd862a67891
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-06 20:52:22 +0600

    break down the java 🍵 program

---
+++ Java/Java Lecture 02/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("break down the java 🍵 program");
+    /*
+     * public : public is an access modifier
+     * class : class is a user defined blueprint with some properties and behaviors
+     * Main : Main is the name of the class
+     * static : static allows to call methods without creating classes
+     * (String[] args) : command line arguments
+     * System.out.println() : it's used to print as output
+     * ; : it means its end of the line / statement
+     */
+  }
+}
