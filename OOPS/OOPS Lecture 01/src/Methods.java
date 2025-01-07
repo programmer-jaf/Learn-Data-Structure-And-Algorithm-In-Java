810a668eeb43de935c7e93a96a199af13298ae09
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-07 21:00:53 +0600

    learn about method and method over-loading

---
+++ OOPS/OOPS Lecture 01/src/Methods.java
@@ -0,0 +1,23 @@
+public class Methods {
+// ! What is method ?
+// * ans : method is a block of code which perform a specific task when it called.
+	public static class Man{
+	// This is method
+	static void sayHello(){
+		System.out.println("👋 hello, this is 👨");
+	}
+	// method over-loading
+	static void sayHello(String name){
+		System.out.println("👋 hello, this is 👨 "+name);
+	}
+}
+	public static void main(String[] args) {
+		System.out.println("learn about methods and methods over-riding");
+		Man me = new Man();
+//	access-methods
+//	Default-method
+		me.sayHello();
+//	Overloaded-method
+		me.sayHello("Jaf");
+	}
+}
