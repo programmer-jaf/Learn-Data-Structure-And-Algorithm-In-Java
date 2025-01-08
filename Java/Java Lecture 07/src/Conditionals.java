f01767cad99b562fa34f3fc7599ebd15167f74e1
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-08 21:03:58 +0600

    learn about conditionals

---
+++ Java/Java Lecture 07/src/Conditionals.java
@@ -0,0 +1,21 @@
+public class Conditionals {
+	public static void main(String[] args) {
+		System.out.println("learn about conditionals");
+//	! what is conditionals ?
+//	* ans : Conditionals are statements that control a program's flow by executing code based on whether a condition is true or false.
+	/*
+		Syntax:
+		if(condition){
+			// ? code
+		}else{
+			// ? code
+		}
+ */
+		int age = 10;
+		if (age>=18){
+			System.out.println("You are adult 👨 now");
+		}else {
+			System.out.println("You are not adult 👦");
+		}
+	}
+}
