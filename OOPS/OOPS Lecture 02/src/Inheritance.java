692c499a509f9c583e317f88ed66ebb6b0f7bedd
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-08 20:06:14 +0600

    learn about Inheritance

---
+++ OOPS/OOPS Lecture 02/src/Inheritance.java
@@ -0,0 +1,13 @@
+public class Inheritance {
+	public class Animal{
+		int legs = 4;
+		int eyes = 2;
+		void walking(){
+			System.out.println("Animal is walking...");
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("learn about Inheritance");
+		
+	}
+}
