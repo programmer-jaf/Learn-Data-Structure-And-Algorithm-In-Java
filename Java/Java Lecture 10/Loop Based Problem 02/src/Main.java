3f2795d1d1fa880775cac89d1079e3c3d2ac7712
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-09 21:57:06 +0600

    solve Loop based Problem 02 Find Factorial

---
+++ Java/Java Lecture 10/Loop Based Problem 02/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Find Factorial");
+		System.out.println();
+		System.out.println("Enter a number");
+		Scanner sc = new Scanner(System.in);
+		int fact = sc.nextInt();
+		int factorial=1;
+		for (int i = fact; i>0 ; i--) {
+			factorial *=i;
+		}
+		System.out.println("The factorial of "+fact+" is "+factorial);
+	}
+}
