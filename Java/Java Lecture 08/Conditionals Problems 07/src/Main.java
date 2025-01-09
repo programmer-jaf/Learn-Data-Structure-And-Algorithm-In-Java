5542b46843b68d61a742fbfb72a59361f6ff8ebf
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-09 20:51:00 +0600

    solve conditional problem 07

---
+++ Java/Java Lecture 08/Conditionals Problems 07/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Guess the Animal program");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter an animal name: Dog 🐶,cat 🐈,rabbit 🐰");
+		String animal = sc.nextLine();
+		if (animal.equals("Dog")) {
+			System.out.println("You have selected Dog 🐶");
+		} else if (animal.equals("cat")) {
+			System.out.println("You have selected cat 🐈");
+		} else if (animal.equals("rabbit")) {
+			System.out.println("You have selected rabbit 🐰");
+		} else {
+			System.out.println("Invalid animal name");
+		}
+		sc.close();
+	}
+}
