c0f6291ab24b7687c8943bd4b81e0c8867e77520
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-31 09:23:19 +0600

    Solve Check Array is Sorted Problem

---
+++ Arrays/Arrays Lecture 01/src/isArraySorted.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class isArraySorted {
+  static boolean isSorted(int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        return false; // Found an unsorted pair
+      }
+    }
+    return true; // If no unsorted pair is found, the array is sorted
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Check if the array is sorted or not");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the size of the array:");
+    int size = sc.nextInt();
+    int[] arr = new int[size];
+
+    System.out.println("Enter the elements of the array:");
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    boolean result = isSorted(arr);
+    System.out.println("The array is sorted: " + result);
+
+    sc.close();
+  }
+}
