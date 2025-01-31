448ff544b28c75ee4b58d3754e91e9509e78b01b
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-31 08:39:42 +0600

    Solve find the second-Smallest element in an Array Problem and fixed some code

--- Arrays/Arrays Lecture 01/src/secondSmallest.java
+++ Arrays/Arrays Lecture 01/src/secondSmallest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class smallestElement {
+public class secondSmallest {
   static int findSmallest(int[] arr){
     int smallest = Integer.MAX_VALUE;
     for (int i = 0; i < arr.length; i++) {
@@ -10,8 +10,18 @@ public class smallestElement {
     }
     return smallest;
   }
+  static int findSecondSmallest(int[] arr){
+    int smallest = findSmallest(arr);
+    int secondsmallest = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i]<secondsmallest && arr[i]!=smallest){
+        secondsmallest=arr[i];
+      }
+    }
+    return secondsmallest;
+  }
   public static void main(String[] args) {
-    System.out.println("Find the smallest element in an array");
+    System.out.println("Find the second smallest element in an array");
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the size of an array");
     int size = sc.nextInt();
@@ -20,8 +30,8 @@ public class smallestElement {
     for (int i = 0; i < arr.length; i++) {
       arr[i] = sc.nextInt();
     }
-    int result = findSmallest(arr);
-    System.out.println("The smallest element in an array "+result);
+    int result = findSecondSmallest(arr);
+    System.out.println("The second smallest value is "+result);
     sc.close();
   }
 }
