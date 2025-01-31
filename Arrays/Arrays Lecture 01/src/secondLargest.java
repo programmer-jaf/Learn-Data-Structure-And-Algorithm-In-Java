80b1bbf37584fd1797b42cf6544d5b6ad0a1a8e8
Programmer-Jaf <programmerjaf@gmail.com>
2025-01-31 08:29:59 +0600

    Solve find the second-Largest element in an Array Problem

---
+++ Arrays/Arrays Lecture 01/src/secondLargest.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class secondLargest {
+  static int findLargest(int[] arr){
+    int largest = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      if (largest<arr[i]) largest=arr[i];
+    }
+    return largest;
+  }
+  static int findSecondLargest(int[] arr){
+    int largest = findLargest(arr);
+    int secondLargest = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i]>secondLargest && arr[i]!=largest) secondLargest=arr[i];
+    }
+    return secondLargest;
+  }
+  public static void main(String[] args) {
+    System.out.println("Find the second largest element in an array");
+    System.out.println("Find the largest element in an array");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of an array");
+    int size = sc.nextInt();
+    int[] arr = new int[size];
+    System.out.println("Enter the elements of an array");
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int result = findSecondLargest(arr);
+    System.out.println("The second Largest element is "+result);
+    sc.close();
+  }
+}
