55c4ce64446b644a56119f09b546c548c7869db3
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-06 12:17:03 +0600

    Learn about Class in Java

---
+++ OOPS/OOPS Lecture 01/src/LearnAboutClass.java
@@ -0,0 +1,14 @@
+public class LearnAboutClass {
+  //  What is Class ?
+  //  Ans: Class is a user-defined blue-print with some attributes and behaviours
+
+//  create a class
+  class Dog{
+    String name;
+    int legs;
+    int eyes;
+}
+  public static void main(String[] args) {
+    System.out.println("learn about class in java");
+  }
+}
